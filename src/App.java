3edd0934858fe62b60cede34340192b7a91ab399
devjoaocosta <joaovitorcostaoliveira@gmail.com>
2026-07-16 21:09:55 -0300

    Anglicizing the code

--- src/App.java
+++ src/App.java
@@ -1,46 +1,42 @@
-
-
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Bem vindo ao programa Calculadora 2000!");
-        int opcao = 0;
-        while (opcao != 5) {
-        
 
-        System.out.println("Seleciona a operaçao que deseja realizar:");
-        System.out.println("1 - Soma");
-        System.out.println("2 - Subtraçao");
-        System.out.println("3 - Multiplicaçao");
-        System.out.println("4 - Divisao");
-        System.out.println("5 - Sair");
-        opcao = Integer.parseInt(System.console().readLine());
-        if (opcao == 5) {
-            System.out.println("Saindo do programa...");
-            break;
-        }
-        else if (opcao < 1 || opcao > 5) {
-            System.out.println("Opçao invalida!");
-        }
-        int a = Integer.parseInt(System.console().readLine("Digite o primeiro numero: "));
-        int b = Integer.parseInt(System.console().readLine("Digite o segundo numero: "));        
-        int result = 0;
-        if (opcao == 1) {
-            result = a + b;
-            System.out.println("O resultado da soma é: " + result);
-        }
-        else if (opcao == 2) {
-            result = a - b;
-            System.out.println("O resultado da subtração é: " + result);
-        }
-        else if (opcao == 3) {
-            result = a * b;
-            System.out.println("O resultado da multiplicação é: " + result);
-        }
-        else if (opcao == 4) {
-            result = a / b;
-            System.out.println("O resultado da divisão é: " + result);
-        }
-        
+  public static void main(String[] args) throws Exception {
+    System.out.println("Welcome to the program Calculadora 2000!");
+    int selectedOperation = 0;
+    while (selectedOperation != 5) {
+      System.out.println("Select the operation you want to perform:");
+      System.out.println("1 - Addition");
+      System.out.println("2 - Subtraction");
+      System.out.println("3 - Multiplication");
+      System.out.println("4 - Division");
+      System.out.println("5 - Exit");
+      selectedOperation = Integer.parseInt(System.console().readLine());
+      if (selectedOperation == 5) {
+        System.out.println("Exiting the program...");
+        break;
+      } else if (selectedOperation < 1 || selectedOperation > 5) {
+        System.out.println("Invalid option!");
+      }
+      int firstNumber = Integer.parseInt(
+        System.console().readLine("Enter the first number: ")
+      );
+      int secondNumber = Integer.parseInt(
+        System.console().readLine("Enter the second number: ")
+      );
+      int result = 0;
+      if (selectedOperation == 1) {
+        result = firstNumber + secondNumber;
+        System.out.println("The result of the addition is: " + result);
+      } else if (selectedOperation == 2) {
+        result = firstNumber - secondNumber;
+        System.out.println("The result of the subtraction is: " + result);
+      } else if (selectedOperation == 3) {
+        result = firstNumber * secondNumber;
+        System.out.println("The result of the multiplication is: " + result);
+      } else if (selectedOperation == 4) {
+        result = firstNumber / secondNumber;
+        System.out.println("The result of the division is: " + result);
+      }
     }
-    }
+  }
 }
